8d79a7f6aabd1f988a371ef2c8c942a0cc630044
CodePoint-46615 <codepoint46615@gmail.com>
2025-09-02 17:32:31 +0600

    reuseable-call-function

--- js/call.js
+++ js/call.js
@@ -1,17 +1,23 @@
-const coinCountField = document.getElementById('count-coin');
+function callFunction(id, msgTitle, msgNumber) {
 
-const msg = document.getElementById('card-title').innerText; 
-const msg2 =document.getElementById('card-1-number').innerText;
+    const coinCountField = document.getElementById(id);
+    const msg = document.getElementById(msgTitle).innerText;
+    const msg2 = document.getElementById(msgNumber).innerText;
+
+    let coinCount = parseInt(coinCountField.textContent, 10);
+
+    if (coinCount < 20) {
+        alert('Insufficient Coin.');
+    }
+    else {
+        let newCoinCount = coinCount - 20;
+        coinCountField.textContent = newCoinCount;
+        alert('Calling ' + msg + ' ' + msg2 + '...');
+    }
+    return coinCountField.textContent;
+}
+
 
 document.getElementById('btn-call-1').addEventListener('click', () => {
-    let coinCount = parseInt(coinCountField.textContent, 10);
-    
-    if(coinCount < 20){
-        alert('Insufficient Coin.'); 
-    }
-    else{
-        let newCoinCount = coinCount - 20; 
-        coinCountField.textContent = newCoinCount;
-        alert(msg3 +'Calling '+ msg +' '+ msg2 + '...'); 
-    }
+    callFunction('count-coin', 'card-title', 'card-1-number');
 });

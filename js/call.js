ae0ea3175969f8c26aa57000c87c2a9fbed7173b
CodePoint-46615 <codepoint46615@gmail.com>
2025-09-02 18:45:10 +0600

    added-call-history-functionality

--- js/call.js
+++ js/call.js
@@ -17,7 +17,43 @@ function callFunction(id, msgTitle, msgNumber) {
     return coinCountField.textContent;
 }
 
+const callHistory = []; 
+
+function Data(){
+    const callHistoryTitle = document.getElementById('call-history-title').innerText; 
+    const callHistoryNumber = document.getElementById('call-history-number').innerText;  
+
+    const data = {
+        title: callHistoryTitle, 
+        number: callHistoryNumber,
+        time: new Date().toLocaleTimeString()
+    }
+
+    callHistory.push(data); 
+}
 
 document.getElementById('btn-call-1').addEventListener('click', () => {
-    callFunction('count-coin', 'card-title', 'card-1-number');
+    callFunction('count-coin', 'card-1-title', 'card-1-number');
+    Data();
+
+    const callLogSection = document.getElementById('call-log-container');
+    callLogSection.innerHTML = ``;
+
+    for (const history of callHistory) {
+        const dynamicCallLogSection = document.createElement('div')
+
+        dynamicCallLogSection.innerHTML =
+        `
+            <div class="flex mt-4 mb-2 justify-between items-center shadow p-2 bg-[#FAFAFA] ">
+                <div>
+                    <h1>${history.title}</h1>
+                    <h1 class="text-[#5C5C5C]">${history.number}</h1>
+                </div>
+                <p>${history.time}</p>
+            </div>
+        `
+        callLogSection.appendChild(dynamicCallLogSection); 
+
+    }
+
 });
